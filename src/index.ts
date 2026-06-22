98e07c8000f4ab132dae64cc70b53c8acd3b85f4
Hirochika Yuda <hyuda@kuropen.me>
2026-06-22 12:04:13 +0900

    Using zod

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import { Hono } from 'hono'
 import { summaly, version } from '@misskey-dev/summaly'
+import * as z from 'zod'
 
 const app = new Hono()
 
+const Options = z.object({
+  userAgent: z.string().optional(),
+  contentLengthLimit: z.int().default(10485760),
+  contentLengthRequired: z.boolean().default(false),
+  responseTimeout: z.int().default(20000),
+  operationTimeout: z.int().default(60000),
+})
+
 app.get('/url', async (c) => {
   const { url, userAgent, timeout, contentLengthLimit, contentLengthRequired } = c.req.query()
-  const summalyOptions = {
+  const summalyOptions = Options.parse({
     userAgent,
-    contentLengthLimit: parseInt(contentLengthLimit || '10485760'),
-    contentLengthRequired: contentLengthRequired === 'true' ? true : false,
-    responseTimeout: parseInt(timeout || '20000'),
-    operationTimeout: parseInt(timeout || '60000'),
-  }
+    contentLengthLimit,
+    contentLengthRequired,
+    responseTimeout: timeout,
+    operationTimeout: timeout,
+  })
   const summalyResponse = await summaly(url, summalyOptions)
   return c.json(summalyResponse)
 })
