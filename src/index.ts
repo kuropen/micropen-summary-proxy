c35e2a63fca0590434b97b4f72f54b52a14b757a
Hirochika Yuda <hyuda@kuropen.me>
2026-06-22 12:22:00 +0900

    Using zod codec

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,17 @@ import * as z from 'zod'
 
 const app = new Hono()
 
+const stringToInt = z.codec(z.string().regex(z.regexes.integer), z.int(), {
+  decode: (str) => parseInt(str),
+  encode: (num) => num.toString(),
+})
+
 const Options = z.object({
   userAgent: z.string().optional(),
-  contentLengthLimit: z.int().default(10485760),
-  contentLengthRequired: z.boolean().default(false),
-  responseTimeout: z.int().default(20000),
-  operationTimeout: z.int().default(60000),
+  contentLengthLimit: stringToInt.default(10485760),
+  contentLengthRequired: z.stringbool().default(false),
+  responseTimeout: stringToInt.default(20000),
+  operationTimeout: stringToInt.default(60000),
 })
 
 app.get('/url', async (c) => {
@@ -17,10 +22,10 @@ app.get('/url', async (c) => {
   const parsedUrl = z.url().parse(url)
   const summalyOptions = Options.parse({
     userAgent,
-    contentLengthLimit: parseInt(contentLengthLimit || '10485760'),
-    contentLengthRequired: contentLengthRequired === 'true' ? true : false,
-    responseTimeout: parseInt(timeout || '20000'),
-    operationTimeout: parseInt(timeout || '60000'),
+    contentLengthLimit,
+    contentLengthRequired,
+    responseTimeout: timeout,
+    operationTimeout: timeout,
   })
   const summalyResponse = await summaly(parsedUrl, summalyOptions)
   return c.json(summalyResponse)
